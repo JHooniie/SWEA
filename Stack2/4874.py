e0a3c7f3ebbf76111ba74a5d15309c5db4cdda24
JHooniie <cliffear324@gmail.com>
2022-11-02 00:15:16 +0900

    221101

--- Stack2/4874.py
+++ Stack2/4874.py
@@ -1,5 +1,56 @@
+# 함수 작성 시 가장 큰 조건을 작성한 후 반복문 작성하기
+def cal(v):
+    #숫자 여부 구분
+    if not v.isdigit():
+        #연산자일 경우
+        while stack:
+            print('wtf : ', stack)
+            # 정상 처리
+            if len(stack) == 1 and v == '.':
+                v = stack.pop()
+                return v
+            # 숫자가 두개 미만일 경우 error
+            # 숫자에 비해 연산자가 많이 남았을 경우
+            elif len(stack) < 2:
+                return 'error'
+            # '.'(결과) 도출 단계에서 스택에 결과값 이외의 값이 있을때
+            # 연산자에 비해 숫자가 많을 경우
+            elif len(stack) >= 2 and v == '.':
+                return 'error'
+            y = stack.pop()
+            x = stack.pop()
+            if v == '+':
+                v = x + y
+            elif v == '-':
+                v = x - y
+            elif v == '*':
+                v = x * y
+            elif v == '/':
+                v = x // y
+            
+            stack.append(v)
+            v = token.pop()
+            print('stack : ', stack)
+            print('token : ', token)
+            print('int v : ', v)
+            return cal(v)
+        return
+    else:
+        stack.append(int(v))
+        v = token.pop()
+        return cal(v)
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    lst = list(map(str, input().split()))
+    token = lst[::-1]
+    stack = []
+    v = token.pop()
+    print('#', test_case, ' ', cal(v), sep='')
+
 # 오답
-# def cal(stack, v):
+# def cal(v):
 #     stack.append(v)
 #     while stack:
 #         v = stack.pop()
